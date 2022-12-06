5d49f711ffd48e6ca62021907974ce58820f36c8
seeljack <seeljack@umich.edu>
2022-12-06 14:31:39 -0500

    JACK

--- js/style.js
+++ js/style.js
@@ -54,3 +54,6 @@ var TxtType = function(el, toRotate, period) {
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
+
+
+    
